f19afb06eb3ba9726853566e6befc1a834b636b6
Syafiq <syafiq.rezpector@gmail.com>
2019-08-29 22:42:37 +0700

    add note entity

---
+++ app/src/main/java/com/github/syafiqq/greendaotest001/entity/Note.java
@@ -0,0 +1,30 @@
+package com.github.syafiqq.greendaotest001.entity;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Index;
+import org.greenrobot.greendao.annotation.NotNull;
+
+import java.util.Date;
+
+import lombok.Data;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity(
+        indexes = {
+                @Index(value = "text, date DESC", unique = true),
+        },
+        generateConstructors = false,
+        generateGettersSetters = false
+)
+@Data
+public class Note {
+    @Id
+    private Long id;
+    @NotNull
+    private String text;
+    private Date date;
+
+    public Note() {
+    }
+}

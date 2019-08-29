060a687e45988c63e45f93294290df595e5a6a40
Syafiq <syafiq.rezpector@gmail.com>
2019-08-29 22:44:24 +0700

    add user entity

---
+++ app/src/main/java/com/github/syafiqq/greendaotest001/entity/User.java
@@ -0,0 +1,24 @@
+package com.github.syafiqq.greendaotest001.entity;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.NotNull;
+
+import lombok.Data;
+
+@Entity(
+        generateConstructors = false,
+        generateGettersSetters = false
+)
+@Data
+public class User {
+    @Id
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String status;
+
+    public User() {
+    }
+}

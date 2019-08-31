dfd077495f746912741c68deedc966a7fdda241f
Syafiq <syafiq.rezpector@gmail.com>
2019-08-31 11:34:07 +0700

    add note test

---
+++ app/src/androidTest/java/com/github/syafiqq/greendaotest001/db/entity/NoteTest.kt
@@ -0,0 +1,79 @@
+package com.github.syafiqq.greendaotest001.db.entity
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.syafiqq.greendaotest001.entity.DaoMaster
+import com.github.syafiqq.greendaotest001.entity.DaoSession
+import com.github.syafiqq.greendaotest001.entity.Note
+import com.github.syafiqq.greendaotest001.entity.NoteDao
+import org.greenrobot.greendao.database.Database
+import org.greenrobot.greendao.identityscope.IdentityScopeType
+import org.hamcrest.core.IsEqual
+import org.hamcrest.core.IsInstanceOf
+import org.hamcrest.core.IsNot
+import org.hamcrest.core.IsNull
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class NoteTest {
+    private var session: DaoSession? = null
+    private var db: Database? = null
+    private var helper: DaoMaster.DevOpenHelper? = null
+    private var context: Context? = null
+    private var dao: NoteDao? = null
+
+    @Before
+    fun setUp() {
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+        helper = DaoMaster.DevOpenHelper(context, "db-test")
+        db = helper?.writableDb
+        session = DaoMaster(db).newSession(IdentityScopeType.Session)
+        dao = session?.noteDao
+
+        assertThat(context, IsNot(IsNull()))
+        assertThat(helper, IsNot(IsNull()))
+        assertThat(db, IsNot(IsNull()))
+        assertThat(session, IsNot(IsNull()))
+        assertThat(dao, IsNot(IsNull()))
+    }
+
+    @After
+    fun tearDown() {
+        dao?.deleteAll()
+        session?.clear()
+        db?.close()
+        helper?.close()
+    }
+
+    @Test
+    fun it_should_select_all_notes() {
+        val notes = dao?.loadAll()
+
+        assertThat(notes, IsInstanceOf(MutableList::class.java))
+    }
+
+    @Test
+    fun it_should_insert_note() {
+        val note = Note().apply {
+            this.id = 1L
+            this.text = "This is text"
+            this.date = Date()
+        }
+
+        dao?.insert(note)
+        val notes = dao?.loadAll()
+
+        assertThat(notes, IsInstanceOf(MutableList::class.java))
+        assertThat(notes?.size, IsEqual(1))
+        val actualNote = notes?.first()
+        assertThat(actualNote?.id, IsEqual(note.id))
+        assertThat(actualNote?.text, IsEqual(note.text))
+        assertThat(actualNote?.date, IsEqual(note.date))
+    }
+}

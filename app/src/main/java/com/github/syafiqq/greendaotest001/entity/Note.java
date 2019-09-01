e7da8170cf58960b1649aa39e4e11686fe1e79c2
Syafiq <syafiq.rezpector@gmail.com>
2019-09-01 17:27:18 +0700

    using green dao generation

--- app/src/main/java/com/github/syafiqq/greendaotest001/entity/Note.java
+++ app/src/main/java/com/github/syafiqq/greendaotest001/entity/Note.java
@@ -1,20 +1,17 @@
 package com.github.syafiqq.greendaotest001.entity;
 
 import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Index;
 import org.greenrobot.greendao.annotation.NotNull;
 
 import java.util.Date;
 
-import org.greenrobot.greendao.annotation.Generated;
-
 @Entity(
         indexes = {
                 @Index(value = "text, date DESC", unique = true),
-        },
-        generateConstructors = false,
-        generateGettersSetters = false
+        }
 )
 public class Note {
     @Id
@@ -23,6 +20,38 @@ public class Note {
     private String text;
     private Date date;
 
+    @Generated(hash = 1501873839)
+    public Note(Long id, @NotNull String text, Date date) {
+        this.id = id;
+        this.text = text;
+        this.date = date;
+    }
+
+    @Generated(hash = 1272611929)
     public Note() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
--- app/src/main/java/com/github/syafiqq/greendaotest001/entity/User.java
+++ app/src/main/java/com/github/syafiqq/greendaotest001/entity/User.java
@@ -1,15 +1,11 @@
 package com.github.syafiqq.greendaotest001.entity;
 
 import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.NotNull;
 
-import org.greenrobot.greendao.annotation.Generated;
-
-@Entity(
-        generateConstructors = false,
-        generateGettersSetters = false
-)
+@Entity
 public class User {
     @Id
     private Long id;
@@ -18,6 +14,38 @@ public class User {
     @NotNull
     private String status;
 
+    @Generated(hash = 1882300885)
+    public User(Long id, @NotNull String name, @NotNull String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    @Generated(hash = 586692638)
     public User() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
